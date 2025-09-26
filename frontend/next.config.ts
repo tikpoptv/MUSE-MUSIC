76c8b8c97b04837a02a5007d615dc39416b8be09
Jedsadaporn pannok <tikpop555@gmail.com>
2025-09-26 09:36:03 +0700

    trun of standalone

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  trailingSlash: false,
+  outputFileTracingRoot: undefined,
 };
 
 export default nextConfig;
