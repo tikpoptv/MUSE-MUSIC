6f6aff05d0c0aebce8f39020b6f0e13cc0443c43
Jedsadaporn pannok <tikpop555@gmail.com>
2025-11-11 14:24:02 +0700

    fix: ปรับ padding ของ admin section ให้เท่ากันและปรับ layout หน้า Song Approved

---
+++ frontend/src/app/admin/manage/page.tsx
@@ -0,0 +1,27 @@
+import AdminMenu from "@/components/AdminMenu";
+import AdminCard from "@/components/AdminCard";
+import ADD_admin from "@/components/ADD_admin";
+import { UserPen } from 'lucide-react';
+
+export default function Page() {
+    return (
+        <AdminMenu>
+            <div className="w-full">
+                <div className="text-xl sm:text-2xl font-semibold flex items-center gap-2 text-violet-600 mb-4 px-2 sm:px-4 md:px-6 lg:px-8 xl:px-10">
+                    <UserPen className="w-10 h-10" />
+                    <span>Song Approved</span>
+                </div>
+
+                <div className="flex flex-wrap justify-center gap-6 mb-6">
+                    <AdminCard name="Tikpop" role="Admin" initials="TP" />
+                    <AdminCard name="Tikpop" role="Admin" initials="TP" />
+                    <AdminCard name="Tikpop" role="Admin" initials="TP" />
+                </div>
+
+                <div className="flex flex-wrap justify-center gap-6 mb-6">
+                    <ADD_admin />
+                </div>
+            </div>
+        </AdminMenu>
+    );
+}
